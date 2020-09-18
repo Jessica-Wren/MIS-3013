294af2d6632632e33a89c952af66eae2a56afa1a
jJessica Wren <jessica.wren@ou.edu>
2020-09-18 14:01:12 -0500

    Completed RandomGuess

--- Partisipations/RandomGuess/Program.cs
+++ Partisipations/RandomGuess/Program.cs
@@ -24,16 +24,36 @@ namespace RandomGuess
 
             Console.WriteLine("Pick a maximum number.");
             maxNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
 
             Console.WriteLine("Your range is " + minNum + " - " + maxNum);
+            Console.WriteLine();
 
             Random rnd = new Random();
             int number = rnd.Next(minNum, maxNum);
 
-            Console.WriteLine(rnd);
+            int iteration = 0;
+            int guess = 0;
 
-            Random rnd = new Random();
-            int number = rnd.Next
+            do
+            {
+                iteration += 1;
+                Console.WriteLine("What is your guess?");
+                Console.WriteLine();
+                guess = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine("The number you picked was " + guess);
+                if (guess != number)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("try again.");
+                }
+            } while (guess != number);
+
+            Console.WriteLine();
+            Console.WriteLine("The number I picked was " + number);
+            Console.WriteLine();
+            Console.WriteLine("You guessed the right number! Yay!");
 
             Console.ReadKey();
         }
