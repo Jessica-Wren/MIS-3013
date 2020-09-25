16a895a5922e4a662ba27b4650bac190609dce81
jJessica Wren <jessica.wren@ou.edu>
2020-09-25 11:48:12 -0500

    String Manipulation Done

---
+++ InClassExamples/StringManipulation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //                         012345678910
+            string instanceOfString = "Hello World!";
+
+
+            instanceOfString = instanceOfString.ToUpper();
+            // Starting value | Condition to loop      | what assignment happens after each loop
+            //                                          this could be i = i + 10 to count by 10 each loop
+            for (int i = 0; i < instanceOfString.Length; i++)
+            {
+                char letter = instanceOfString[i];
+                // Have to conver the CHARACTER to a string to use the String methods
+                Console.WriteLine(letter);//.ToString().ToUpper());
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
