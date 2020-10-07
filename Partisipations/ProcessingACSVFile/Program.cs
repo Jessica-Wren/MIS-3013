3253cd2a703e02d7b404a1bbd39ec88711e49d5a
jJessica Wren <jessica.wren@ou.edu>
2020-10-07 16:53:10 -0500

    added List example in the in class example Collections

---
+++ Partisipations/ProcessingACSVFile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace ProcessingACSVFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\jessi\Downloads\sales_data_sample.csv";
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
