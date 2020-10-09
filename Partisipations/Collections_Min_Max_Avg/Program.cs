12924c80fdb11115ee3c8f9b27ffdc46dc06c729
jJessica Wren <jessica.wren@ou.edu>
2020-10-08 21:17:33 -0500

    started and finshed participation collections_min_max_avg

---
+++ Partisipations/Collections_Min_Max_Avg/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections_Min_Max_Avg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter all of your exam grades.");
+
+            List<double> examScores = new List<double>();
+
+            string response = "yes";
+            do
+            {
+                Console.WriteLine("Enter one of your exam grades.");
+                examScores.Add(Convert.ToDouble(Console.ReadLine()));
+
+                Console.WriteLine("Do you have any more grades to enter? Yes or No?");
+                response = Console.ReadLine();
+            } while (response.ToLower() == "yes");
+
+
+            double max = 0;
+            double min = 101;
+            double average = 0;
+            int i = 0;
+            foreach (double scores in examScores)
+            {
+                i++;
+                if (scores > max)
+                {
+                    max = scores;
+                }
+                if (scores < min)
+                {
+                    min = scores;
+                }
+                average += scores;
+            }
+
+            average = average / i;
+
+            Console.WriteLine();
+            Console.WriteLine("Your maximum score was " + max.ToString() + ".");
+            Console.WriteLine("Your minimum score was " + min.ToString() + ".");
+            Console.WriteLine("Your average score was " + average.ToString() + ".");
+
+            Console.ReadKey();
+        }
+    }
+}
