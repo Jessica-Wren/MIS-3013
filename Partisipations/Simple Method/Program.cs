7816baa176fb0d382bb9ba23b9388529fa17fe4b
jJessica Wren <jessica.wren@ou.edu>
2020-10-20 17:44:16 -0500

    Started and finished Simple Method participation

---
+++ Partisipations/Simple Method/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Simple_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Speak();
+            
+
+            Console.ReadKey();
+        }
+
+        static void Speak ()
+        {
+            string response = "";
+            Console.WriteLine("Please enter dog, monkey, or goat.");
+            response = Console.ReadLine();
+            Console.WriteLine();
+
+            if (response.ToLower() == "dog")
+            {
+                Console.WriteLine("bark!");
+            }
+            else if (response.ToLower() == "monkey")
+            {
+                Console.WriteLine("oo oo ah ah!");
+            }
+            else
+            {
+                Console.WriteLine("baaaa!");
+            }
+        }
+    }
+}
