30a10ec185726a9ea4fc357acff4c3e6eca359da
jJessica Wren <jessica.wren@ou.edu>
2020-11-05 16:20:43 -0600

    Started ClassesVsParallelCollections

---
+++ InClassExamples/ClassesVsParallelCollections/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesVsParallelCollections
+{
+    public class Person
+    {
+
+        public string FirstName { get; set; }
+        public string LastName {get; set;}
+        public string DateOfBirth {get; set;}
+        public string FavoriteFood {get; set;}
+        public double AccountBalance {get; set;}
+        //if its private then you do not need get and set
+
+        //constructor (ctor tab tab) is basically a method that has two things about it. 1: the name of the constuctor is the same name as the class (person). 2. no return type
+        public Person()
+        {
+
+        }
+
+    }
+}
---
+++ InClassExamples/ClassesVsParallelCollections/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesVsParallelCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> firstNames = new List<string>();
+            List<string> lastNames = new List<string>();
+            List<string> dobs = new List<string>();
+            List<string> favFood = new List<string>();
+            List<double> accountBalance = new List<double>();
+
+            firstNames.Add("Jessica");
+            lastNames.Add("Wren");
+            dobs.Add("11/05/2020");
+            favFood.Add("Pizza");
+            accountBalance.Add(500);
+
+            firstNames.Add("Cathy");
+            lastNames.Add("Salvo");
+            dobs.Add("12/25/2020");
+            favFood.Add("Mango");
+            accountBalance.Add(1000);
+
+            for (int i = 0; i < firstNames.Count; i++)
+            {
+                Console.WriteLine(firstNames[i] + " " + lastNames[i] + " " + dobs[i] + " likes " + favFood[i] + " and has account balance of " + accountBalance[i].ToString("c") + ".");
+            }
+
+
+
+
+
+            Console.WriteLine("Press any key to exit");
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
