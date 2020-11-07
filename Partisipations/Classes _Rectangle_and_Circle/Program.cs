76ab541fce8c376a7934bb694b98c36fbaba5ce1
jJessica Wren <jessica.wren@ou.edu>
2020-11-06 18:04:28 -0600

    Started (P) Classes - Rectangle & Circle

---
+++ Partisipations/Classes _Rectangle_and_Circle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Classes__Rectangle_and_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+            
+
+            //1.15
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Partisipations/Classes _Rectangle_and_Circle/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes__Rectangle_and_Circle
+{
+    public class Rectangle
+    {
+        public double Length;
+        public double Width;
+
+        //path A
+        //empty/default constructor
+        public Rectangle()
+        {
+            Length = 0;
+            Width = 0;
+        }
+
+        public double CalculateArea() 
+        {
+            double areaCalculation = Length * Width;
+            return areaCalculation;
+        }
+
+        public double CalculatePerimeter() 
+        {
+            double perimeterCalculation = 2*(Length + Width);
+            return perimeterCalculation;
+        }
+    }
+}
