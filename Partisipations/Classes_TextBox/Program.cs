a246ee32e3e29ef52e577d3a3dbfc4b8d600548e
jJessica Wren <jessica.wren@ou.edu>
2020-11-18 12:09:34 -0600

    Started Classes TexBox participation

---
+++ Partisipations/Classes_TextBox/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Classes_TextBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextBox Textbox1 = new TextBox();
+            Textbox1.Padding = 10;
+            Textbox1.Text = "Hi, im Textbox1.";
+
+            Console.WriteLine();
+
+            TextBox Textbox2 = new TextBox();
+            Textbox2.Padding = 50;
+            Textbox2.Text = "Hi, I am Texbox2";
+
+            
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Partisipations/Classes_TextBox/TextBox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+
+namespace Classes_TextBox
+{
+    class TextBox
+    {
+        public int Padding { get; set; }
+        public ConsoleColor BackColor { get; set; }
+        public ConsoleColor ForeColor { get; set; }
+        public string Text { get; set; }
+
+        public TextBox()
+        {
+            Padding = 0;
+            /*BackColor = ConsoleColor.
+            ForeColor = ConsoleColor.*/
+            Text = string.Empty;
+        }
+
+        public void DisplayText() 
+        {
+            
+        }
+    }
+}
